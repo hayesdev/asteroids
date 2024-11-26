d8f16304a14abf329d91a972180fee1a0859d779
Greg Hayes <gthelight@gmail.com>
2024-11-26 10:52:23 -0700

    player model now drawn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 # import constants
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,17 +9,23 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    running = True
 
-    while True:
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         # fill() expects a tuple so be sure to use ()
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
