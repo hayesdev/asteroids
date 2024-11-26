eaa935358a0767cbceeb1efa0e753aac0e9437ad
Greg Hayes <gthelight@gmail.com>
2024-11-26 14:48:52 -0700

    adds groups, fix player movement

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     dt = 0
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
@@ -27,9 +32,17 @@ def main():
 
         # fill() expects a tuple so be sure to use ()
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        # player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
+        # player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 
